3721280c6fa639ab1d94c76d5260bc6983724bbd
vinodkumarDV <dvvinod143@gmail.com>
2022-01-16 19:22:17 +0530

    completed 1c

---
+++ firstC.js
@@ -0,0 +1,22 @@
+// 1.c.After cleaning the text in the sentence from question number b you will get the following text.Count the number of words in this text.Don't include words with only one letter.
+
+//     ```js
+// const sentence =  `I am a teacher and I love teaching.There is nothing as more rewarding as educating and empowering people.I found teaching more interesting than any other jobs.Does this motivate you to be a teacher ? `
+// console.log(countWords(sentence));
+// 31
+
+// ```
+
+function countWords(sentence) {
+    let res = [];
+    res1 = sentence.split(' ');
+    for (let i = 0; i < res1.length; i++){
+        if (res1[i].length > 1) {
+            res.push(res1[i]);
+        }
+    }
+    return res.length;
+}
+
+const sentence = `I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?`;
+console.log(countWords(sentence));
