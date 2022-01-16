f8b7ab7d49aa7d52e4deba67a36a37c541660949
vinodkumarDV <dvvinod143@gmail.com>
2022-01-16 19:47:44 +0530

    completed 2A

---
+++ secondA.js
@@ -0,0 +1,16 @@
+// 2.a.Looping a triangle: Write a loop that makes seven calls to console.log to output the following triangle:
+// ```js
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+// ```
+
+var x = '';
+for (let i = 0; i > 12; i++){
+    x = '#' + x;
+    console.log(x)
+}
