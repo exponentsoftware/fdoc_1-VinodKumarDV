abfc02ac4665487180085de42d80ad3a60b56d27
vinodkumarDV <dvvinod143@gmail.com>
2022-01-16 19:29:10 +0530

    completed 1D

---
+++ firstD.js
@@ -0,0 +1,20 @@
+// 1.d.How many words were used to construct this sentence.Now, don't exclude one letter words.
+//     ```js
+// console.log(varietyOfWords(sentence))
+// 28
+// ```
+
+function countWords(sentence) {
+    let res = [];
+    res1 = sentence.split(' ');
+    for (let i = 0; i < res1.length; i++) {
+
+        res.push(res1[i]);
+
+    }
+    return res.length;
+}
+const sentence = `I am a teacher and I love teaching. 
+There is nothing as more rewarding as educating and empowering people.
+ I found teaching more interesting than any other jobs.`;
+console.log(countWords(sentence));
